1b45ec4819537f83a120ed04309fdaeceb3dc2bb
FaHaIk <fabian.ikizoglu@web.de>
2021-08-22 20:24:20 +0200

    Commit made from VS Code Azure Static Web Apps

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
-import React from 'react';
+import './App.css';
+import React from 'react'
+import 'antd/dist/antd.css';
+import { Input, Radio, DatePicker, Checkbox } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [visibility, setVisibility] = React.useState(1);
+  const [isDatepickerDisabled, setIsDatepickerDisabled] = React.useState(true);
+
+  const onChangeVisibility = e => {
+    console.log('radio checked', e.target.value);
+    setVisibility(e.target.value);
+  };
+
+  const onChangeHasExpirationDate = e => {
+    setIsDatepickerDisabled(e.target.checked)
+  }
+
+  return (
+    <div className="App">
+      <div className="center-wrapper">
+        <Input size="large" placeholder="Paste long URL and shorten it!" prefix={<SearchOutlined />} />
+        <div className="spacer"></div>
+        <div className="settings-wrapper">
+          <div style={{ alignSelf: "center" }}>
+            <Radio.Group onChange={onChangeVisibility} value={visibility}>
+              <Radio value={1}>Public</Radio>
+              <Radio value={2}>Private</Radio>
+            </Radio.Group>
+          </div>
+          <div>
+            <Checkbox defaultChecked onChange={onChangeHasExpirationDate}>Never Expire</Checkbox>
+            <DatePicker
+              disabled={isDatepickerDisabled}
+              placeholder="Expiration date"
+              disabledDate={(current) => {
+                return current.valueOf() < Date.now();
+              }}
+              bordered={false}
+            />
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
 }
 
 export default App;
